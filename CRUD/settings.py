3b57f100b41a28c815cf2ada437149f92f890691
Gabriel <al180213@alumnos.uacj.mx>
2023-02-14 09:02:59 -0700

    add allowed host

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5_6rdno7jl+u0i^a*^tp9o1da^8jd8)%l*2mr9&s921jatrd&_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['irads2023-gabriel180213-production.up.railway.app']
 
 
 # Application definition
